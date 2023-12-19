3b833f337047f5271120cc14375c16d4751c2351
Shrabon Talukder <shrabontalukder425@gmail.com>
2023-12-19 13:15:12 +0600

    Complete This Project

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,27 +1,83 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "../../Firebase/firebase.config";
+import { useState } from "react";
 
 const Login = () => {
+  const [user, setUser] = useState({});
   const auth = getAuth(app);
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider();
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
       .then((result) => {
-        const user = result.user;
-        console.log(user)
+        const userLoggedIn = result.user;
+        setUser(userLoggedIn);
       })
-      .catch(error => {
-        console.log("error", error.message)
+      .catch((error) => {
+        console.log("error", error.message);
+      });
+    console.log(user);
+  };
+  const handleGithub = () => {
+    signInWithPopup(auth, githubProvider)
+      .then((result) => {
+        const userLoggedIn = result.user;
+        setUser(userLoggedIn);
+      })
+      .catch((error) => {
+        console.log("error", error.message);
+      });
+    console.log(user);
+  };
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
       });
   };
   return (
-    <div className="w-[fit-content] hover:border-error hover:bg-slate-700 duration-1000 bg-cyan-900 border p-20 rounded mx-auto my-20">
-      <button
-        onClick={handleGoogleSignIn}
-        className="btn btn-outline btn-error"
-      >
-        Login with Google
-      </button>
+    <div className=" container border p-20 rounded mx-auto my-20">
+      <div className="text-center">
+        {user ? (
+          <button
+            onClick={handleSignOut}
+            className="btn btn-outline btn-neutral"
+          >
+            Sign Out
+          </button>
+        ) : (
+          <div className="flex justify-center gap-4">
+            <button
+              onClick={handleGoogleSignIn}
+              className="btn btn-outline  btn-error"
+            >
+              Login with Google
+            </button>
+            <button
+              onClick={handleGithub}
+              className="btn btn-outline btn-accent"
+            >
+              Github Login
+            </button>
+          </div>
+        )}
+      </div>
+      {user && (
+        <div>
+          <h3 className="text-2xl font-bold">User : {user.displayName}</h3>
+          <h4>UserE-mail : {user.email}</h4>
+          <img className="rounded-full" src={user.photoURL} alt="" />
+        </div>
+      )}
     </div>
   );
 };
